0491319a69d25ada32439c03bd938a5e3e11dfb9
victoria-umoh <victoriasuave07@gmail.com>
2024-03-04 23:06:53 +0100

    LAD

--- routes/web.php
+++ routes/web.php
@@ -14,42 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-})->name('index');
-
-Route::get('/login', function (){
-    return view('login');
-})->name('login');
-Route::get('/register', function (){
-    return view('register');
-})->name('register');
-Route::get('/user',function(){
-    return view('user_form');
-})->name('user');;
-Route::get('/agent-list',function(){
-    return view('agent_list');
-})->name('agent-list');
-Route::get('/shortlet', function(){
-    return view('shortlet');
-})->name('shortlet');
-Route::get('/userdash', function(){
-    return view('user_dash');
-})->name('user_dash');
-Route::get('/userprofile', function(){
-    return view('user_profile');
-})->name('user_profile');
-Route::get('/landlorddash', function(){
-    return view('landlord_dash');
-})->name('landlord_dash');
-Route::get('/layout', function(){
-    return view('layouts/other_dashboard');
-})->name('layout');
-Route::get('/profile', function(){
-    return view('profile');
-})->name('profile');
-Route::get('/teammembers', function(){
-    return view('team_members');
-})->name('team_members');
-Route::get('/editteam', function(){
-    return view('edit_team');
-})->name('edit_team');
+    return view('welcome');
+});
