ee117775f964b3e7cbd2e42b0a620ee560bb36ac
victoria-umoh <victoriasuave07@gmail.com>
2024-03-05 15:56:39 +0100

    added usertype to users table to dev

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,8 @@ return new class extends Migration
             $table->string('password');
             $table->string('phone', 20);
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
@@ -36,5 +38,5 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-    
+
 };
